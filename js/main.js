837673c5ae276617bca8e1c4f79a553c2da2a76b
Vinicius Savegnago <viniciussave@gmail.com>
2019-12-13 15:57:46 -0300

    contact page

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Vanishing Scroll Icon when scrolling page
+
+// Caching
+const scroll_icon = document.querySelector('.scroll-icon')
+
+window.onscroll = () =>{
+
+    scroll_icon.style.opacity = 0
+
+}
