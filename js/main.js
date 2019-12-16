694cd5275fae6b1cdf652a3b57e741c98ec6e5b2
Vinicius Savegnago <viniciussave@gmail.com>
2019-12-16 17:18:56 -0300

    portfolio done (adjust send message)

--- js/main.js
+++ js/main.js
@@ -3,8 +3,43 @@
 // Caching
 const scroll_icon = document.querySelector('.scroll-icon')
 
+const menu_icon = document.querySelector('.menu-icon')
+const close_icon = document.querySelector('.close-icon')
+const nav = document.querySelector('nav')
+
+const nav_links = document.querySelectorAll('.nav-link')
+
+// Scroll Icon
 window.onscroll = () =>{
 
     scroll_icon.style.opacity = 0
 
 }
+
+// Responsive navbar Toggle
+// Open
+menu_icon.addEventListener('click', () =>{
+
+    nav.classList.toggle('nav-closed')
+
+})
+
+// Close
+close_icon.addEventListener('click', () =>{
+
+    nav.classList.toggle('nav-closed')
+
+})
+
+// navbar links 
+
+nav_links.forEach((link) => {
+
+    link.addEventListener('click', () => {
+
+        console.log('link')
+        nav.classList.toggle('nav-closed')
+
+    })
+
+})
