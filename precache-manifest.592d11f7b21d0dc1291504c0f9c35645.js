866cd536bd0bdd95e5f0d2be8e8461c90e51eb60
savegnago <savegdesigner@gmail.com>
2022-04-05 17:51:32 -0300

    Updates

---
+++ precache-manifest.592d11f7b21d0dc1291504c0f9c35645.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7dab35b2c75e79a28d8ad44b2179764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08a0c5bd982cd9d92c7a",
+    "url": "/static/js/2.064affd0.chunk.js"
+  },
+  {
+    "revision": "bed72d581679ed9ba32cbac65f9bfa7e",
+    "url": "/static/js/2.064affd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a48b8b6ed7b925ef77",
+    "url": "/static/js/main.ad617c2e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "8215747e80f031c2bb5d21e2ea49857d",
+    "url": "/static/media/code-icon.8215747e.svg"
+  },
+  {
+    "revision": "e612fa4e02101d2b961b0acb5c7ec4da",
+    "url": "/static/media/farfetch-project.e612fa4e.png"
+  },
+  {
+    "revision": "258fab4d737070bc9cdf9d0b7a15e3ac",
+    "url": "/static/media/goventures-project.258fab4d.png"
+  },
+  {
+    "revision": "38b78a953345eb9731dae6e669fafe2a",
+    "url": "/static/media/happy-project.38b78a95.png"
+  },
+  {
+    "revision": "2bacc51bb66e4d6e2ebcbbeedbe87427",
+    "url": "/static/media/logo.2bacc51b.svg"
+  },
+  {
+    "revision": "7a8f2de91c398df8b64f019fceb87ca3",
+    "url": "/static/media/me.7a8f2de9.png"
+  },
+  {
+    "revision": "6ee71b5ba92e041bc1d377f2b433f571",
+    "url": "/static/media/node-icon.6ee71b5b.svg"
+  },
+  {
+    "revision": "67ddc0e3ea0f5ac41008890e1b06985d",
+    "url": "/static/media/prototype-icon.67ddc0e3.svg"
+  },
+  {
+    "revision": "51d270a67a7037f754b632ed9c269735",
+    "url": "/static/media/react-icon.51d270a6.svg"
+  },
+  {
+    "revision": "1a059d6600b9001a8a9a1925ee37f349",
+    "url": "/static/media/rocket-icon.1a059d66.svg"
+  },
+  {
+    "revision": "4ce92645c2b655f77d96a00970fe58f2",
+    "url": "/static/media/ts-icon.4ce92645.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.96d47ad312bd54a36e9bcbe9b0d6264f.js"
+  "/precache-manifest.592d11f7b21d0dc1291504c0f9c35645.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
